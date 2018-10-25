0b0e6821c09e72eeb1c21d6f1f378346e2000ea2
Sunny <sunni98@abv.bg>
2018-10-25 16:01:03 +0300

    Rearranged repo by separating it into 2 folders. Also added hw for week 3 and 4 in 2nd course folder.

---
+++ II-nd Course/WebApplicationCRUD/WebApplicationCRUD/Pages/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationCRUD.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public CreateModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            //Customer customer = new Customer();
+            //customer.Name = Name;
+            //customer.Id = 1;
+            _db.Customers.Add(Customer);
+            await _db.SaveChangesAsync();
+
+            Message = $"Customer {Customer.Name} added.";
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
